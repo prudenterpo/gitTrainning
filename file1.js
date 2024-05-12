e2ce95f83639946d42e5e223d53634dde35f1a53
rodrigopdo <rodrigo.prudente82@gmail.com>
2024-05-12 10:07:58 -0300

    commit bla

--- file1.js
+++ file1.js
@@ -1,13 +1,4 @@
-Comit 1m
 
-commit 2
-commit 3
-commit da feat1 para remote master
 
-outro commit para tentar rebase
 
-commit feat2 1
-commit feat2 2
-commit feat2 3
-
-commit da feat2 para remote master
+Mudanças de teste na feat1 para conflitão...
