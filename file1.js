b110b754fcff4de0e5fcacf0db9f7f7218d11854
rodrigopdo <rodrigo.prudente82@gmail.com>
2024-05-12 10:01:55 -0300

    novo commitão

--- file1.js
+++ file1.js
@@ -1,13 +1,1 @@
-Comit 1m
-
-commit 2
-commit 3
-commit da feat1 para remote master
-
-outro commit para tentar rebase
-
-commit feat2 1
-commit feat2 2
-commit feat2 3
-
-commit da feat2 para remote master
+Novas edições para testar os conflitos.
